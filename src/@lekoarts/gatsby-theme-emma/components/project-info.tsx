3d6a12082ef34faca2b3dad1f6f34b2bd0a08372
Manuel Correia <manuel.correia2004@gmail.com>
2022-04-26 07:48:36 +0100

    Co-authored-by: JoaoOIiveira <JoaoOIiveira@users.noreply.github.com>

---
+++ src/@lekoarts/gatsby-theme-emma/components/project-info.tsx
@@ -0,0 +1,13 @@
+/** @jsx jsx */
+
+type ProjectInfoProps = {
+  project: {
+    client: string
+    date: string
+    service: string
+  }
+}
+
+const ProjectInfo = ({ project }: ProjectInfoProps) => null
+
+export default ProjectInfo
